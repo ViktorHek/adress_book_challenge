b4ede3175e3f1c633d65c09cefe1447b199fafe1
viktor <viktorkarlsson125@gmail.com>
2020-10-19 12:57:39 +0200

    lade till kontakten Thomas med uppgifter samt ett test för att se Thomas

--- cypress/integration/userCanCreateAContact.feture.js
+++ cypress/integration/userCanCreateAContact.feture.js
@@ -3,5 +3,11 @@ describe("user can create a contact", () => {
     it("test", () => {
         cy.visit("http://localhost:3002")
         cy.get("#add-contact").click()
+        cy.get("#name").type("Thomas")
+        cy.get("#email").type("thomas@craft.se")
+        cy.get("#phone").type("0700 101010")
+        cy.get("#company").type("Craft Academy")
+        cy.get("#notes").type("Awesome coder")
+        cy.get("#twitter").type("@thomasochman")
     })
 })
